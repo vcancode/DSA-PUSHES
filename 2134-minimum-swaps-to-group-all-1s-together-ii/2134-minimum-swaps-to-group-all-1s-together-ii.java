991823b88955e0b8ce1d4b38daa57167ab548c92
Code_reaper_vineet <techyvineet352@gmail.com>
2026-08-24 11:25:23 +0530

    Time: 3 ms (99.35%), Space: 90.4 MB (51.96%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.java
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.java
@@ -1,19 +1,28 @@
 class Solution {
     public int minSwaps(int[] nums) {
-    int ones = 0, x = 0, onesInWindow = 0, i = 0, n = nums.length;
-    for (int v: nums)
-        if (v == 1) ones++;
-    int nums2[] = new int[n * 2]; // since circular array we are just duplicating the array so that last idx can connect to 1st
-
-    // now fill the duplicate array
-    for (i = 0; i < n * 2; i++)
-        nums2[i] = nums[i % n];
-
-    for (i = 0; i < n * 2; i++) {
-        if (i >= ones && nums2[i - ones] == 1) x--; // i-ones is left pointer
-        if (nums2[i] == 1) x++;
-        onesInWindow = Math.max(x, onesInWindow);
+        int totalOnes = 0;
+        for (int num : nums) {
+            totalOnes += num;
+        }
+        
+        if (totalOnes == 0 || totalOnes == nums.length) {
+            return 0;
+        }
+        
+        int n = nums.length;
+        int currentOnes = 0;
+        
+        for (int i = 0; i < totalOnes; i++) {
+            currentOnes += nums[i];
+        }
+        
+        int maxOnes = currentOnes;
+        for (int i = 0; i < n; i++) {
+            currentOnes -= nums[i];
+            currentOnes += nums[(i + totalOnes) % n]; //** imp line we are %ing the idx so it circles back to 0 */
+            maxOnes = Math.max(maxOnes, currentOnes);
+        }
+        
+        return totalOnes - maxOnes;
     }
-    return ones - onesInWindow;
-}
 }

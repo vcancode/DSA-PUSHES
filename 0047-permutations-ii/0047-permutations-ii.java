055faa5695d5eaabc9900033a0ac46243f729441
Code_reaper_vineet <techyvineet352@gmail.com>
2026-08-28 10:32:57 +0530

    Time: 2 ms (99.03%), Space: 47.2 MB (6.03%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,77 +1,52 @@
-// my bruteforce approach
-
-// class Solution {
-//     private void helper(int[] nums, List<List<Integer>> ans,List<Integer> comb,Set<Integer> st ){
-//         if(comb.size()==nums.length){
-//             if(!ans.contains(comb)){
-//             ans.add(new ArrayList<>(comb));
-//             return;
-//             }
-//         }
-
-//         for(int i=0;i<nums.length;i++){
-//             if(!st.contains(i)){
-//                 st.add(i);
-//                 comb.add(nums[i]);
-//                 helper(nums,ans,comb,st);
-//                 st.remove(i);
-//                 comb.remove(comb.size()-1);
-//             }
-//         }
-
-
-
-
-
-//     }
-//     public List<List<Integer>> permuteUnique(int[] nums) {
-//         List<List<Integer>> ans = new ArrayList<>();
-//         List<Integer> comb = new ArrayList<>();
-//         Set<Integer> st = new HashSet<>();
-//         helper(nums,ans,comb,st);
-//         return ans;
-//     }
-// }
-
-
 class Solution {
-    public List<List<Integer>> permuteUnique(int[] nums){
-        List<List<Integer>> res = new ArrayList<>();
-        solve(0, nums, res);
-        return res;
-    }
-    void solve(int s, int[] nums, List<List<Integer>> res){
-        if(s == nums.length - 1){
-            ArrayList<Integer> newAr = new ArrayList<>();
-            for(int val: nums){
-                newAr.add(val);
+
+    private void helper(int[] nums,
+                        List<List<Integer>> ans,
+                        int idx) {
+
+        if (idx == nums.length) {
+            List<Integer> permutation = new ArrayList<>();
+
+            for (int num : nums) {
+                permutation.add(num);
             }
-            res.add(newAr);
+
+            ans.add(permutation);
             return;
         }
-        for (int i = s; i < nums.length; i++) {
-            boolean duplicate = false;
 
-            for (int j = s; j < i; j++) {
-                if (nums[j] == nums[i]) {
-                    duplicate = true;
-                    break;
-                }
-            }
+        // Values already tried for this position
+        Set<Integer> st = new HashSet<>();
 
-            if (duplicate) {
+        for (int i = idx; i < nums.length; i++) {
+
+            int chosen = nums[i];
+
+            // Don't make the same choice twice at this level
+            if (st.contains(chosen)) {
                 continue;
             }
 
-            int temp = nums[s];
-            nums[s] = nums[i];
+            st.add(chosen);
+
+            // swap
+            int temp = nums[idx];
+            nums[idx] = nums[i];
             nums[i] = temp;
 
-            solve(s + 1, nums, res);
+            // recursive call
+            helper(nums, ans, idx + 1);
 
-            temp = nums[s];
-            nums[s] = nums[i];
+            // backtrack
+            temp = nums[idx];
+            nums[idx] = nums[i];
             nums[i] = temp;
         }
+    }
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(nums, ans, 0);
+        return ans;
     }
 }

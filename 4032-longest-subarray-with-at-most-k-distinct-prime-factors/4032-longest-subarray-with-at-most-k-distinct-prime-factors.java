560a361ee819d6c81e70ad96e33ca100310cdce0
Code_reaper_vineet <techyvineet352@gmail.com>
2026-08-31 07:54:59 +0530

    Time: 533 ms (8.25%), Space: 97.4 MB (43.33%) - LeetHub

--- 4032-longest-subarray-with-at-most-k-distinct-prime-factors/4032-longest-subarray-with-at-most-k-distinct-prime-factors.java
+++ 4032-longest-subarray-with-at-most-k-distinct-prime-factors/4032-longest-subarray-with-at-most-k-distinct-prime-factors.java
@@ -1,49 +1,46 @@
 class Solution {
     public int longestSubarray(int[] nums, int k) {
         int n = nums.length;
-        List<List<Integer>> arr = new ArrayList<>();
-        for(int num : nums)
-            {
-                List<Integer> list = new ArrayList<>();
-                for(int i=2;i*i<=num;i++)
-                    {
-                        if(num%i==0)
-                        {
-                            list.add(i);
-                            while(num%i==0)
-                                {
-                                    num /= i;
-                                }
-                        }
-                    }
-                if(num > 1)list.add(num);
-                arr.add(list);
-            }
-        int l = 0;
-        int r = 0;
-        Map<Integer,Integer> map = new HashMap<>();
-        int ans = 0;
-        while(r<arr.size())
-            {
-                for(int i=0;i<arr.get(r).size();i++)
-                    {
-                        int num = arr.get(r).get(i);
-                        map.put(num,map.getOrDefault(num,0)+1);
-                    }
-                while(map.size()>k)
-                {
-                    for(int i=0;i<arr.get(l).size();i++)
-                    {
-                        int num = arr.get(l).get(i);
-                        map.put(num,map.getOrDefault(num,0)-1);
-
-                        if(map.get(num)==0)map.remove(num);
-                    }
-                    l++;
+        Map<Integer,List<Integer>> mp = new HashMap<>();
+        for(int x:nums){
+            int num=x;
+            List<Integer> li = new ArrayList<>();
+            for(int i=2;i*i<=num;i++){
+                if(num%i==0){
+                   li.add(i);
+                   while(num%i==0){
+                    num/=i;
+                   }
+                   
                 }
-                ans = Math.max(ans,r-l+1);
-                r++;
             }
+
+            if(num>1) li.add(num);
+            mp.put(x,li);
+        }
+
+        int l=0,r=0;
+        int ans=0;
+        Map<Integer,Integer> mp2 = new HashMap<>();
+        while(r<nums.length){
+            for(int i=0;i<mp.get(nums[r]).size();i++){
+                int num=mp.get(nums[r]).get(i);
+                mp2.put(num,mp2.getOrDefault(num,0)+1);
+            }
+
+            while(mp2.size()>k){
+                for(int i=0;i<mp.get(nums[l]).size();i++){
+                    int num=mp.get(nums[l]).get(i);
+                    mp2.put(num,mp2.getOrDefault(num,0)-1);
+                    if(mp2.get(num)==0) mp2.remove(num);
+                }
+                l++;
+            }
+            ans=Math.max(ans,r-l+1);
+            r++;
+        }
+
         return ans;
+        
     }
 }

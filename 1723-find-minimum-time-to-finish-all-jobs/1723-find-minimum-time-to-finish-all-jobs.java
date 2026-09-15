918fa2d3a68f1e8053958372dd1e0641b2d1b799
Code_reaper_vineet <techyvineet352@gmail.com>
2026-09-15 11:37:01 +0530

    Time: 2 ms (99.75%), Space: 43.2 MB (41.58%) - LeetHub

--- 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.java
+++ 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.java
@@ -1,74 +1,66 @@
+import java.util.*;
+
 class Solution {
-
-    int ans = Integer.MAX_VALUE;
-
     public int minimumTimeRequired(int[] jobs, int k) {
 
-        // 1. Largest jobs first
         Arrays.sort(jobs);
-        reverse(jobs);
 
-        int[] workers = new int[k];
+        int n = jobs.length;
 
-        backtrack(jobs, 0, workers);
+        int low = jobs[n - 1];
+        int high = 0;
 
-        return ans;
-    }
-
-    void backtrack(int[] jobs, int idx, int[] workers) {
-
-        // All jobs assigned
-        if (idx == jobs.length) {
-
-            int max = 0;
-
-            for (int time : workers) {
-                max = Math.max(max, time);
-            }
-
-            ans = Math.min(ans, max);
-
-            return;
+        for (int job : jobs) {
+            high += job;
         }
 
-        int job = jobs[idx];
+        while (low < high) {
+
+            int mid = low + (high - low) / 2;
+
+            // Fresh workers for every check
+            int[] workers = new int[k];
+
+            if (canAssign(jobs, n - 1, workers, mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    private boolean canAssign(int[] jobs, int index,
+                              int[] workers, int limit) {
+
+        // All jobs assigned
+        if (index < 0) {
+            return true;
+        }
+
+        int job = jobs[index];
 
         for (int i = 0; i < workers.length; i++) {
 
-            // 2. Branch pruning
-            if (workers[i] + job >= ans) {
-                continue;
+            if (workers[i] + job <= limit) {
+
+                workers[i] += job;
+
+                if (canAssign(jobs, index - 1, workers, limit)) {
+                    return true;
+                }
+
+                // Backtrack
+                workers[i] -= job;
             }
 
-            // Choose
-            workers[i] += job;
-
-            // Explore
-            backtrack(jobs, idx + 1, workers);
-
-            // Undo
-            workers[i] -= job;
-
-            // 3. Symmetry pruning
+            // Empty workers are equivalent
             if (workers[i] == 0) {
                 break;
             }
         }
-    }
 
-    void reverse(int[] arr) {
-
-        int left = 0;
-        int right = arr.length - 1;
-
-        while (left < right) {
-
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-
-            left++;
-            right--;
-        }
+        return false;
     }
 }

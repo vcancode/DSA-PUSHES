aba4d61f447d78b2e9c02944f73f0638162daecb
Code_reaper_vineet <techyvineet352@gmail.com>
2026-09-15 10:14:35 +0530

    Time: 6 ms (82.6%), Space: 43 MB (83.41%) - LeetHub

---
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    private boolean helper(int[] matchsticks,int[] buckets,int target,int idx){
+        if(idx<0) return true;
+        int num = matchsticks[idx];
+        for(int i=0;i<buckets.length;i++){
+            if(num+buckets[i]>target) continue;
+
+            buckets[i]+=num;
+
+            if(helper(matchsticks,buckets,target,idx-1)) return true;
+
+            buckets[i]-=num;
+
+            if(buckets[i]==0) break;
+        }
+
+        return false;
+    }
+    public boolean makesquare(int[] matchsticks) {
+        int n=matchsticks.length;
+        int sum = 0;
+        for(int num:matchsticks){
+            sum+=num;
+        }
+        if(sum%4!=0) return false;
+
+        int target = sum/4;
+
+        Arrays.sort(matchsticks);
+
+        int[] buckets = new int[4];
+
+        return helper(matchsticks,buckets,target,n-1);
+    }
+}
